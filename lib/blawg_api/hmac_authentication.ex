4370c3bcb1bcea7a38bdb7fb97eae652fd94148c
Denver Smith <denverpsmith@gmail.com>
2019-07-11 20:36:48 -0600

    Refactor HMAC Authentication Plug Test
    - Utilize pattern matching and pipe operator
    - Add more test cases
    - Move repetative logic into setup function

--- lib/blawg_api/hmac_authentication.ex
+++ lib/blawg_api/hmac_authentication.ex
@@ -11,17 +11,12 @@ defmodule BlawgApi.HmacAuthentication do
 
   @impl BlawgApi.Authentication
   def should_permit_access?(conn, hmac_digest) do
-    auth_headers = Plug.Conn.get_req_header(conn, "authorization")
-
-    if Enum.count(auth_headers) > 0 do
-      [_proto, hmac_message] =
-        auth_headers
-        |> List.first()
-        |> String.split(" ")
-
-      Plug.Crypto.secure_compare(hmac_digest, hmac_message)
-    else
-      false
-    end
+    conn
+    |> Plug.Conn.get_req_header("authorization")
+    |> get_hmac_from_authorization_header()
+    |> Plug.Crypto.secure_compare(hmac_digest)
   end
+
+  defp get_hmac_from_authorization_header(["hmac " <> hmac]), do: hmac
+  defp get_hmac_from_authorization_header(_), do: ""
 end
